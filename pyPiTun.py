e76e486ef1a39e6b00e441578510a20fc512780f
orangmiliter <42175785+orangmiliter@users.noreply.github.com>
2021-04-28 01:29:32 +0700

    Create pyPiTun.py

---
+++ pyPiTun.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+from keys import keys
+import requests
+import subprocess
+
+login_url = "https://www.pitunnel.com/login"
+host_url = "https://www.pitunnel.com/active_table"
+headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+}
+
+ses = requests.Session()
+data = {
+    "email": keys['email'],
+    "password": keys['password'],
+    "commit": "Login"
+}
+
+login = ses.post(login_url, data=data, headers=headers)
+get_host = ses.get(host_url, headers=headers)
+# print(get_host.text)
+bs = BeautifulSoup(get_host.text, 'lxml')
+tBody = bs.find('tbody')
+td_Search = tBody.find_all('tr', class_=None)
+for td_row in td_Search:
+    td_kolom = td_row.findAll('td')
+    out = td_kolom[1].text.replace('\n', "").replace("                                                                                      ","").replace("                                                                                  ","")#IniBiarPanjang
+    td_split = out.strip('\n').split(':')
+    print("pi@{} -p{}".format(td_split[0], td_split[1]))
